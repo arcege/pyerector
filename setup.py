18a08fa4b0cc07fadd088445fedbb2a94190c059
arcege <arcege@localhost>
2012-07-23 20:59:37 +0000

    Add support files.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# Copyright @ 2012 Michael P. Reilly. All rights reserved.
+
+from distutils.core import setup
+
+setup(name="pyerector",
+      version="0.5",
+      description="Self-contained Python build library",
+      author="Michael P. Reilly",
+      author_email="arcege@gmail.com",
+      long_description="Python module to create simplified build scripts",
+      url="http://code.google.com/p/pyerector",
+      py_modules=["pyerector"],
+      license=["LICENSE"],
+      data_files=["LICENSE"],
+    )
