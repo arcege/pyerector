a34944ac66696e34e966031144c51f21ca27007d
Michael P. Reilly <arcege@gmail.com>
2013-04-21 23:24:20 -0400

    Support unittests better.

--- pyerector/vcs/__init__.py
+++ pyerector/vcs/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import os
+
 from .git import *
 from .mercurial import *
 from .subversion import *
@@ -14,9 +16,11 @@ __all__ = [
     'VCS'
 ]
 
+basedir = os.curdir
+
 def VCS(*args, **kwargs):
     for vcs in vcs_set:
-        if vcs.vcs_check():
+        if vcs.vcs_check(dir=basedir):
             break
     else:
         raise RuntimeError('no version control found')
