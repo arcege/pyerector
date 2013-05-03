baed64250b8d09b0ac647da754f277ed62e70c1d
Michael P. Reilly <arcege@gmail.com>
2013-05-03 10:03:59 -0400

    Move comment for clarity.

--- pyerector/targets.py
+++ pyerector/targets.py
@@ -28,9 +28,9 @@ class Help(Target):
     """This information"""
     def run(self):
         for name, obj in sorted(registry.get('Target').items()):
+            if name[1:].lower() !+ name[1:]:
+                continue  # ignore non-callable targets
             # use display.write to get around --quiet option
-            if name[1:].lower() != name[1:]:
-                continue
             if hasformat:
                 display.write('{0:20}  {1}'.format(
                         obj.__name__.lower(),
