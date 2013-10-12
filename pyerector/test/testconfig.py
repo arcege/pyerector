7772122ff71d997e69050187daf996c470e85954
Michael P. Reilly <arcege@gmail.com>
2013-10-12 12:52:08 -0400

    Catch deprecation when testing Config class.

--- pyerector/test/testconfig.py
+++ pyerector/test/testconfig.py
@@ -2,6 +2,7 @@
 # Copyright @ 2012-2013 Michael P. Reilly. All rights reserved.
 
 import os
+import warnings
 
 from .base import *
 
@@ -19,5 +20,10 @@ class TestConfig(TestCase):
             V['basedir'] = curdir
     def test_property_get(self):
         c = Config()
-        self.assertEqual(V['basedir'], c.basedir)
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always', DeprecationWarning)
+            self.assertEqual(V['basedir'], c.basedir)
+            assert len(w) == 1
+            assert issubclass(w[-1].category, DeprecationWarning)
+            assert "deprecated" in str(w[-1].message)
 
