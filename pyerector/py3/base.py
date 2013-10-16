822a04ac13d0f118321b77e5cc0b61002b383d3e
Michael P. Reilly <arcege@gmail.com>
2013-10-16 14:42:18 -0400

    Remove unused method.

--- pyerector/py3/base.py
+++ pyerector/py3/base.py
@@ -6,13 +6,5 @@ from ..exception import Error
 from ..metaclass import IniterMetaClass
 
 class Base(metaclass = IniterMetaClass):
-    def rewrap_exception(self):
-        e = sys.exc_info()
-        if isinstance(e[1], Error):
-            msg = '%s:%s' % (self, e[1].args[0])
-            v =  e[1].args[1]
-        else:
-            msg = str(self)
-            v = e[1]
-        raise Error(msg, v) from e[1]
+    pass
 
