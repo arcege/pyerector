3620e8220712685a56df0cab70f062af7329d040
Michael Reilly <arcege@gmail.com>
2016-12-27 21:48:14 +0000

    bitbucket-pipelines.yml created online with Bitbucket

---
+++ pyerector/config.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# Copyright @ 2012-2016 Michael P. Reilly. All rights reserved.
+"""Handle configuration values.
+
+The Config class managed "variables" before global variables were
+introduced.  This class has now been deprecated and exists only for
+backward compatibility.
+
+The State class is to give the same functionality as the now deprecated
+Verbose class.  The "noop" and "noTimer" instances are initialized here
+and turned on in PyErector.arguments().
+"""
+
+from warnings import warn
+
+from .variables import V
+
+__all__ = [
+    'Config',
+    'noop',
+    'noTimer',
+]
+
+
+class Config(object):
+    """Deprecated, but still provide the same interface, but to variables."""
+    def __getattr__(self, variable):
+        warn('Config has been deprecated', DeprecationWarning)
+        return V[variable]
+
+    def __setattr__(self, variable, value):
+        warn('Config has been deprecated', DeprecationWarning)
+        V[variable] = value
+
+
+class State(object):
+    """Create a mutable boolean object."""
+    def __init__(self, initial=False):
+        self.state = initial
+
+    def __bool__(self):
+        return self.state
+    __nonzero__ = __bool__
+
+    def on(self):
+        """Change state to True."""
+        self.state = True
+
+    def off(self):
+        """Change state to False."""
+        self.state = False
+
+noop = State()
+
+# display timing information, changed in pyerector.main.PyErector
+noTimer = State()
+
---
+++ pyerector/test/testconfig.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# Copyright @ 2012-2016 Michael P. Reilly. All rights reserved.
+
+import os
+import warnings
+
+try:
+    from .base import *
+except ValueError:
+    import sys
+    sys.path.insert(
+        0,
+        os.path.normpath(
+            os.path.join(
+                os.path.dirname(__file__), os.pardir, os.pardir
+            )
+        )
+    )
+
+PyVersionCheck()
+
+from pyerector.config import Config
+from pyerector.variables import V
+
+
+class TestConfig(TestCase):
+    def setUp(self):
+        self.thisdir = os.path.realpath(os.curdir)
+        self.dir = self.dir.real
+        if 'basedir' not in V:
+            from os import curdir
+            V['basedir'] = Path(curdir)
+
+    def test_property_get(self):
+        c = Config()
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always', DeprecationWarning)
+            self.assertEqual(V['basedir'], c.basedir)
+            assert len(w) == 1
+            assert issubclass(w[-1].category, DeprecationWarning)
+            assert "deprecated" in str(w[-1].message)
+
+if __name__ == '__main__':
+    import logging, unittest
+    logging.getLogger('pyerector').level = logging.ERROR
+    unittest.main()
